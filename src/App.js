c8373c0b8a6ec2515e6d025df4ca070e783ecfa9
Naviralobo <majel.josephine>
2022-10-10 21:01:00 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import SignUp from "./Profile/SignUp";
+import { useState } from "react";
+import SignUp from "./components/Profile/SignUp";
+
+import Welcome from "./Welcome";
 
 function App() {
-  return <SignUp />;
+  const [isLogin, setIsLogin] = useState(false);
+  const loginHandler = () => {
+    setIsLogin(true);
+  };
+  return (
+    <>
+      {!isLogin && <SignUp onLogin={loginHandler} />}
+      {isLogin && <Welcome />}
+    </>
+  );
 }
 
 export default App;
---
+++ src/Welcome.js
@@ -0,0 +1,6 @@
+const Welcome = () => {
+  return (
+    <h1 style={{ textAlign: "center" }}>Welcome to ExpenseTracker Page!!</h1>
+  );
+};
+export default Welcome;
